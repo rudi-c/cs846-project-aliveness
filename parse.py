14af84237ec72e16462574b2210217e435060db9
Rudi Chen <rudichen@gmail.com>
2016-03-12 22:17:56 -0500

    Handle full project and revision information.

--- parse.py
+++ parse.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
+import argparse
 import math
 import os
 import re
 import sqlite3
 import sys
 
-def parse_projects(c):
+def parse_projects(c, filename):
     """
     Expect lines of the format:
     o[project id] = name|revision count|has docs
 
     (Output of project-repository.boa)
     """
-    with open("projects-small.txt") as f:
+    with open(filename) as f:
         for line in f.readlines():
             matches = re.match(r'o\[(.*)\] = (.*)\|(.*)|(.*)', line)
             project_id = int(matches.group(1))
@@ -25,14 +26,14 @@ def parse_projects(c):
                               for v in (project_id, name, rev_count, has_docs))
             c.execute("INSERT INTO repos VALUES (%s)" % fields)
 
-def parse_revisions(c):
+def parse_revisions(c, filename):
     """
     Expect lines of the format:
     o[project id][revision hash] = date|author
 
     (Output of revisions.boa)
     """
-    with open("revisions-small.txt") as f:
+    with open(filename) as f:
         for line in f.readlines():
             matches = re.match(r'o\[(.*)\]\[(.*)\] = (.*)\|(.*)', line)
             project_id = int(matches.group(1))
@@ -44,7 +45,7 @@ def parse_revisions(c):
                               for v in (revision_hash, project_id, date, author))
             c.execute("INSERT INTO revisions VALUES (%s)" % fields)
 
-def parse_span():
+def parse_span(filename):
     """
     Except lines of the format:
     counts[span] = #
@@ -54,7 +55,7 @@ def parse_span():
     print "Project lifespans in days"
 
     results = []
-    with open("activity-span.txt") as f:
+    with open(filename) as f:
         for line in f.readlines():
             matches = re.match(r'counts\[(.*)\] = (.*)', line)
             span = int(matches.group(1))
@@ -82,10 +83,24 @@ def parse_span():
         print ("[%d, %d]: " % (start, start * 2)).ljust(16) + str(count)
 
 
-def main(args):
-    os.remove('repos.db')
+def main():
+    # Command-line arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--full', action="store_true")
+    args = parser.parse_args()
 
-    conn = sqlite3.connect('repos.db')
+    if args.full:
+        database_name = "repos.db"
+    else:
+        database_name = "repos_test.db"
+
+    # Recreate from scratch.
+    try:
+        os.remove(database_name)
+    except OSError:
+        pass
+
+    conn = sqlite3.connect(database_name)
     c = conn.cursor()
 
     c.execute('''CREATE TABLE repos
@@ -93,12 +108,16 @@ def main(args):
     c.execute('''CREATE TABLE revisions
                  (id, project, date, author)''')
 
-    parse_span()
-    parse_projects(c)
-    parse_revisions(c)
+    parse_span("activity-span.txt")
+    if args.full:
+        parse_projects(c, "projects.txt")
+        parse_revisions(c, "revisions.txt")
+    else:
+        parse_projects(c, "projects-small.txt")
+        parse_revisions(c, "revisions-small.txt")
 
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
