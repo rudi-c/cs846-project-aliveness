8abfdfe425db2dd1eec1fe294a12430af9a96387
Rudi Chen <rudichen@gmail.com>
2016-03-12 19:16:05 -0500

    Parse basic information about projects.

--- parse.py
+++ parse.py
@@ -5,10 +5,29 @@ import re
 import sqlite3
 import sys
 
-def parse_revision_dates(c):
+def parse_projects(c):
     """
     Expect lines of the format:
-    o[project id][revision hash] = date
+    o[project id] = name|revision count
+
+    (Output of project-repository.boa)
+    """
+    with open("projects-small.txt") as f:
+        for line in f.readlines():
+            matches = re.match(r'o\[(.*)\] = (.*)\|(.*)', line)
+            project_id = int(matches.group(1))
+            name = matches.group(2)
+            rev_count = int(matches.group(3))
+
+            fields = ",".join('"' + str(v) + '"'
+                              for v in (project_id, name, rev_count))
+            c.execute("INSERT INTO repos VALUES (%s)" % fields)
+
+
+def parse_revisions(c):
+    """
+    Expect lines of the format:
+    o[project id][revision hash] = date|author
 
     (Output of revisions.boa)
     """
@@ -31,11 +50,12 @@ def main(args):
     c = conn.cursor()
 
     c.execute('''CREATE TABLE repos
-                 (id)''')
+                 (id, name, revision_count)''')
     c.execute('''CREATE TABLE revisions
                  (id, project, date, author)''')
 
-    parse_revision_dates(c)
+    parse_projects(c)
+    parse_revisions(c)
 
     conn.commit()
     conn.close()
