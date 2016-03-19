7316a182d60e51bc2b9209cfc6ff200e703221f8
Rudi Chen <rudichen@gmail.com>
2016-03-19 18:23:09 -0400

    Also generate log plots when applicable.

--- analysis.py
+++ analysis.py
@@ -98,6 +98,22 @@ def compute_feature_vectors(db_connection, feature_functions, nobt):
 
     return feature_vector, label_vector
 
+def plot_binary_vs_continuous(name, continuous, binary, log):
+    # Begin new plot (needed since plt is stateful)
+    fig = plt.figure()
+    # Plot data points with some jittering to see the overlapping ones
+    plt.plot(continuous, binary, 'ro', alpha=0.05)
+    # Plot slighly above 1.0 to see things better.
+    plt.ylim(-0.1, 1.1)
+    if log:
+        plt.xscale('log')
+        # Save to file
+        plt.savefig(PLOTS_DIR + "_log_" + name + ".png")
+    else:
+        # Save to file
+        plt.savefig(PLOTS_DIR + name + ".png")
+    plt.close(fig)
+
 def main():
     # Command-line arguments.
     parser = argparse.ArgumentParser()
@@ -142,19 +158,19 @@ def main():
         os.makedirs(PLOTS_DIR)
 
     for feature_name, feature_column in features_by_name.items():
-        print "Plotting " + feature_name
+        print "Plotting %s - range [%f, %f]" % (feature_name, min(feature_column), max(feature_column))
         feature_range = max(feature_column) - min(feature_column)
         features_jitter = (np.random.rand(len(labels)) - 0.5) * feature_range / 100
         jittered_features = np.array(feature_column) + features_jitter
         jittered_labels = np.array(labels) + (np.random.rand(len(labels)) - 0.5) * 0.05
-        # Begin new plot (needed since plt is stateful)
-        plt.figure()
-        # Plot data points with some jittering to see the overlapping ones
-        plt.plot(jittered_features, jittered_labels, 'ro', alpha=0.05)
-        # Plot slighly above 1.0 to see things better.
-        plt.ylim(-0.1, 1.1)
-        # Save to file
-        plt.savefig(PLOTS_DIR + feature_name + ".png")
+
+        plot_binary_vs_continuous(feature_name, jittered_features, jittered_labels, False)
+
+        # Not point in doing a log plot for data in [0, 1]
+        if max(feature_column) > 1:
+            # It's common to plot log(x + 1) to deal with x = 0
+            adjusted_features = jittered_features + np.ones(len(jittered_features))
+            plot_binary_vs_continuous(feature_name, adjusted_features, jittered_labels, True)
 
 
 if __name__ == "__main__":
