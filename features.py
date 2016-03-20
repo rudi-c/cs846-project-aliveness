ea4f97718dc6dfdf992159de8d0bd902de0b9b94
Rudi Chen <rudichen@gmail.com>
2016-03-20 15:03:32 -0400

    We already moved this function to another file.

--- features.py
+++ features.py
@@ -20,37 +20,6 @@ import feature_functions
 BACKTESTING_DAYS = 30
 PLOTS_DIR = "plots/"
 
-def time_since_last_commit_distribution(db_connection):
-    print """Distribution of projects according to time of last commit in
-          days before cutoff date"""
-    projects = get_projects(db_connection)
-
-    print "Total number of projects: %d" % len(projects)
-
-    days_before_cutoff = []
-    for i, project in enumerate(projects):
-        revisions = get_revisions_for_project(db_connection, project.id)
-        revisions = get_revisions_before_cutoff(revisions, CUTOFF_DATE)
-
-        # Make sure we have revisions at all
-        if len(revisions) > 0:
-            last_date = revisions[-1].date
-            assert last_date < CUTOFF_DATE
-            difference = (CUTOFF_DATE - last_date).days
-            days_before_cutoff.append(difference)
-
-        if i % 100 == 0:
-            # Using a carriage return allows the terminal to override
-            # the previous line, making it more like a progress effect.
-            print >> sys.stderr, ("%d percent of projects processed\r" %
-                                   int(float(i) / len(projects) * 100)),
-
-    print "Finished"
-
-    bins = log_bin(days_before_cutoff)
-    for (start, end), count in bins:
-        print ("[%d, %d]:" % (start, end)).ljust(16) + str(count)
-
 def compute_feature_vectors(db_connection, feature_functions, nobt):
 
     projects = get_projects(db_connection)
